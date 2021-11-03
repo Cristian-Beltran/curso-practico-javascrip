4d8243f352f52cb98784778f38df082f6ebfca12
Cristian Beltran <cristian.beltran.paco@gmail.com>
2021-11-03 10:45:56 -0400

    Creacion de proyecto de formas

---
+++ figuras geomtricas/main.js
@@ -0,0 +1,9 @@
+// Codigo de calculo del cuadrado
+const ladoCuadrado = 5;
+console.log(`Los lado del cuadrado miden: ${ladoCuadrado} cm`);
+
+const perimetroCuadrado = ladoCuadrado*4;
+console.log(`El perimetro del cuadrado es: ${perimetroCuadrado} cm`);
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log(`El area del cuadrado es: ${areaCuadrado} cm²`);
