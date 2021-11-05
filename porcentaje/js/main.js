b7d939739d33f03ef8eb75423867d6ad05d25626
Cristian Beltran <cristian.beltran.paco@gmail.com>
2021-11-05 12:05:26 -0400

    Añadido funcionalidad con javascript

--- porcentaje/js/main.js
+++ porcentaje/js/main.js
@@ -1,1 +1,8 @@
-const precioFinal = (precio,descuento) => (precio * (100-descuento))/100;
+const totalPrice = (price,discount) => (price * (100-discount))/100;
+
+const result = document.getElementById('result');
+function calculate(){
+    const price = parseInt(document.getElementById("price").value); 
+    const discount = parseInt(document.getElementById("discount").value);
+    result.innerText = `${totalPrice(price,discount).toFixed(2)}$`;
+}
